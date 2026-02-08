48ad3e7a55419a4b22a3356930d61430503b82a0
admin <dbochoa77@proton.me>
2026-02-07 17:12:01 -0800

    tmux auto boot works

--- home/features/xinitric/default.nix
+++ home/features/xinitric/default.nix
@@ -2,11 +2,10 @@
 
 {
   programs.bash.enable = true;
-
   programs.bash.bashrcExtra = ''
-    if [ -z "$TMUX" ] && [ -n "$SSH_TTY" ] && command -v tmux >/dev/null; then
-      tmux attach -t main || tmux new -s main
-    fi
+  	if [ -z "$TMUX" ] && command -v tmux >/dev/null; then
+  	  tmux attach -t main || tmux new -s main
+	fi
   '';
 }
 
